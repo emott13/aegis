b7b7f2cf2e534e993474d95f3217fd36507000a7
bluemario8 <bluemario812@gmail.com>
2025-11-21 08:43:05 -0500

    Added ScheduleSeeder. All seeders are complete now

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             EmployeeSeeder::class,
             AppointmentSeeder::class,
             CareSeeder::class,
+            ScheduleSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
