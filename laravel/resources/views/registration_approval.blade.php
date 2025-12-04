2944e3e440c6ff9937d9471c203b3b638fe2ccf7
bluemario8 <bluemario812@gmail.com>
2025-12-04 11:24:57 -0500

    Added data onto the registration approval page and fixed an issue with the Users being called in routes/api.php instead of UserController

--- laravel/resources/views/registration_approval.blade.php
+++ laravel/resources/views/registration_approval.blade.php
@@ -4,11 +4,73 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>Register Approval</title>
+    {{-- temp styles. Should go in file --}}
+    <style>
+        .row > * {
+            background-color: lightgray;
+            border: 1px solid black;
+        }
+    </style>
 </head>
 @section('content')
 <body>
-    <h1>Registration Approval</h1>
+    <h1 class="text-center">Registration Approval</h1>
+    
+    <form class="container">
+        <input class="col" type="submit" value="Submit">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Role</th>
+                    <th scope="col">Approve?</th>
+                </tr>
+
+                @foreach ($unapproved as $user)
+                    <tr id="{{ $user['user_id'] }}">
+                        <th scope="col">{{ $user['fname'] }}</th>
+                        <th scope="col">{{ $user['lname'] }}</th>
+                        <th scope="col">{{ ucwords($user['role_name']) }}</th>
+                        <th scope="col">
+                            <input type="checkbox" 
+                                name="yes_{{ $user['user_id'] }}"
+                            >Yes</input>
+                            <input type="checkbox" 
+                                name="no_{{ $user['user_id'] }}"
+                            >No</input>
+                        </th>
+                    </tr>
+                @endforeach
+            </thead>
+        </table>
+    </form>
+    {{-- <form class="container" action="" method="">
+        <div class="row">
+            <div class="col">
+                Name
+            </div>
+            <div class="col">
+                Role
+            </div>
+            <div class="col">
+                Approved?
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                Someone
+            </div>
+            <div class="col">
+                Someone
+            </div>
+            <div class="col">
+            </div>
+        </div>
+        </div>
+    </form> --}}
 </body>
 @endsection
 </html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -18,9 +18,11 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', [HomeController::class, 'home'])->name('home.index');
+Route::get('/', [HomeController::class, 'home'])->name('home.index')->middleware('auth');
 Route::get('/login', [LoginController::class, 'loginPage'])->name('login');
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::get('/register-approval', [RegisterController::class, 'approvalPage'])->name('approval')->middleware('auth');
+
 
 Route::post('/register', [RegisterController::class, 'store'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
