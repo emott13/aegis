e39fc1a5d080b1b5bfd88c3ccad08599b37a0fb9
EllieMott <elizabethmott1326@gmail.com>
2025-12-07 12:20:11 -0500

    form input adjustment

--- laravel/resources/views/patient_home.blade.php
+++ laravel/resources/views/patient_home.blade.php
@@ -6,10 +6,9 @@
     <h1 class="container" style="font-weight: 600; text-align: center; color: white;">Patient Home</h1>
     {{-- send form to patients.php for handling --}}
     <form action="{{ route('patient') }}"> 
-        <input type="number" name="pid" id="pid" placeholder="Patient ID" required>
-        <input type="text" name="fid" id="fid" placeholder="Family Code" required>
-        <input type="date" name="date" id="date" value="{{ $today }}">
+        <input type="number" name="pid" id="pid" placeholder="Patient ID" readonly>
         <input type="text" name="p_name" id="p_name" placeholder="Patient Name" readonly>
+        <input type="date" name="date" id="date" value="{{ $today }}">
     </form>
 @if ($patients->count())
     <table class="table table-sm table-striped table-hover table-border">
