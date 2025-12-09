63ef8ceb7c36a8cd4ace0e89ee87e58fed1005b8
bluemario8 <bluemario812@gmail.com>
2025-12-09 16:50:59 -0500

    Login now requires approved accounts

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -15,7 +16,8 @@ class AuthController extends Controller
             'password' => 'required|string|max:255',
         ]);
 
-        if (Auth::attempt($validated))
+        // check if it is a valid user and it is validated
+        if (Auth::attempt($validated) && User::where('email', '=', $validated['email'])->get('approved')[0]['approved'] == '1')
         {
             $request->session()->regenerate();
             
