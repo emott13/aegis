03de9ce3643f9e2cb10d1cdf3f05f73352103f61
EllieMott <elizabethmott1326@gmail.com>
2026-01-10 12:21:48 -0500

    updated access role migration up()

--- laravel/database/migrations/2025_11_13_144709_create_access_roles_table.php
+++ laravel/database/migrations/2025_11_13_144709_create_access_roles_table.php
@@ -7,29 +7,35 @@ use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up(): void                                                      // run the migrations
     {
         Schema::create('access_roles', function (Blueprint $table) {
             $table->id('role_id');
-            $table->string('role_name', 20)->unique();               // increase allows for growth
+            $table->string('role_name', 20)->unique();              // increase allows for growth
+
             $table->timestamps();
         });
 
-        DB::statement(                                              // postgresql -- start IDs at 1000
-            "
-            ALTER TABLE access_roles
-            ALTER COLUMN role_id RESTART WITH 1000
-            "
-        );
+        // -- Set starting ID for roles to 1000 -- //
+        //
+        // check the database driver
+        // switch to set the sequence accordingly
+
+        switch (DB::getDriverName()){
+            case 'pgsql':                                                           // PostgreSQL
+                DB::statement("ALTER SEQUENCE access_roles_role_id_seq RESTART WITH 1000;");
+                break;
+            case 'mysql':                                                           // MySQL
+                DB::statement("ALTER TABLE access_roles AUTO_INCREMENT = 1000;");
+                break;
+            default:
+                throw new RuntimeException(('Unsupported database driver: ' . DB::getDriverName()));
+        }
+
+        DB::statement("ALTER SEQUENCE access_roles_role_id_seq RESTART WITH 1000;"); // start IDs at 1000
         
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('access_roles');
