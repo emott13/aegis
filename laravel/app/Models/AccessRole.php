514f5fd8aaa607cbc78b7c57e22295c1bb1f8b28
EllieMott <elizabethmott1326@gmail.com>
2026-01-11 10:52:58 -0500

    +users relationship in AccessRole Model

--- laravel/app/Models/AccessRole.php
+++ laravel/app/Models/AccessRole.php
@@ -3,6 +3,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AccessRole extends Model
 {
@@ -13,5 +14,16 @@ class AccessRole extends Model
     protected $fillable = [
         'role_name'
     ];
+
+    // -- Relationships -- //
+
+    public function users (): HasMany
+    {
+        return $this->hasMany(
+            related:    User::class, 
+            foreignKey: 'role_id', 
+            localKey:   'role_id'
+        );
+    }
 }
 ?>
