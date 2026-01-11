3896c8d73a9b53f5df29358e913166c44774e621
EllieMott <elizabethmott1326@gmail.com>
2026-01-11 11:03:49 -0500

    adjustments to User Model

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -4,27 +4,19 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use App\Models\AccessRoles;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    protected $table = 'users';                                 // table associated with the model
+    protected $primaryKey = 'user_id';                          // primary key associated with the table
 
-    /**
-     * The primary key associated with the table.
-     * @var string
-     */
-    protected $primaryKey = 'user_id';
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
+    protected $fillable = [                                     // attributes that are mass assignable
         'fname',
         'lname',
         'dob',
@@ -35,51 +27,57 @@ class User extends Authenticatable
         'role_id'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
+    protected $hidden = [                                       // attributes that should be hidden for serialization   
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
+    protected $casts = [                                        // attributes that should be cast
         'password' => 'hashed',
+        'approved' => 'boolean',
+        'dob' => 'date'
     ];
-    // relational functions
-    public function patient()
+    
+
+    // -- Relational Functions -- //
+
+    public function role(): BelongsTo
     {
-        return $this->hasOne(Patient::class, 'user_id', 'user_id');
-    }
-    public function accessRoles()
-    {
-        return $this->hasOne(AccessRole::class, 'role_id', 'role_id');
+        return $this->belongsTo(
+            related:    AccessRole::class, 
+            foreignKey: 'role_id', 
+            ownerKey:   'role_id');
     }
 
-    public function employee()
+    public function patient(): HasOne
     {
-        return $this->hasOne(Employee::class, 'user_id', 'user_id');
+        return $this->hasOne(
+            related:    Patient::class, 
+            foreignKey: 'user_id', 
+            localKey:   'user_id');
     }
-    // getter functions
-    public function getFullNameAttribute()
+
+    public function employee(): HasOne
+    {
+        return $this->hasOne(
+            related:    Employee::class, 
+            foreignKey: 'user_id', 
+            localKey:   'user_id');
+    }
+
+    // -- Getter Functions -- //
+    public function getFullNameAttribute(): string
     {
         return "{$this->fname} {$this->lname}";
     }
 
-    public function getRoleName()
+    public function getRoleName(): mixed
     {
-        return AccessRole::where('role_id', $this->role_id)->get('role_name')[0]['role_name'];
+        return AccessRole::where(column: 'role_id', operator: $this->role_id)->get(columns: 'role_name')[0]['role_name'];
     }
 
-    public function getAccessLevel()
+    public function getAccessLevel(): mixed
     {
-        return AccessRole::where('role_id', $this->role_id)->get('access_level')[0]['access_level'];
+        return AccessRole::where(column: 'role_id', operator: $this->role_id)->get(columns: 'access_level')[0]['access_level'];
     }
 }
