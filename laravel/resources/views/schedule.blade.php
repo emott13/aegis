7129a04042afe46bde7ba31f44a4d35e8bd38e8b
EllieMott <elizabethmott1326@gmail.com>
2026-01-22 16:43:00 -0500

    css adjustments schedule page

--- laravel/resources/views/schedule.blade.php
+++ laravel/resources/views/schedule.blade.php
@@ -1,46 +1,52 @@
 @extends('layouts.app')
-
+<style>
+    .tableWrapper{
+        border-radius: 25px;
+        background-color: #1c0032ee;
+        width: 80%; 
+        margin: 10px auto;
+        padding: 1rem 2rem;
+    }
+    .textColor{
+        color: white;
+    }
+</style>
 @section('content')
 <h1 class="container" style="font-weight: 600; text-align: center; color: white;">Schedule</h1>
 
 @if ($schedules->count())
-    <div style="width: 100%; padding: 0 10vw">
+    <div>
         @foreach ($schedules as $schedule)
-            <div>
-                <p>Date: {{ $schedule->schedule_date }}</p>
-                <p>Created by:
+        {{-- <hr> --}}
+            <div class="tableWrapper">
+                <p class="textColor">Date: {{ $schedule->schedule_date }}</p>
+                <p class="textColor">Created by:
                     {{-- {{ $schedule->creator?->user->fname }} --}}
                     {{-- {{ $schedule->creator?->user->lname }} --}}
                 </p>
-            </div>
-            <hr>
-            @foreach ($schedule->assignments as $assignment)
-            <table class="table table-sm table-striped table-hover table-border container">
-                <tr>
-                    <th>Shift</th>
-                    <th>Role</th>
-                    <th>Care Group</th>
-                    <th>Employee Name</th>
-                </tr>
-                <tr>
-                    <td>{{ strtoupper($assignment->shift) }}</td>
-                    <td>{{ $assignment->role }}</td>
-                    <td>{{ $assignment->care_group }}</td>
-                    <td>
-                        {{ $assignment->employee->user->fname }}
-                        {{ $assignment->employee->user->lname }}
-                    </td>
-                </tr>
-            </table>
-                {{-- <p>
+            
+                <table class="table table-sm table-striped table-hover table-border">
                     
-                    — 
-                    
-                </p>
-                <p>{{ $assignment->schedule_id }}</p>
-                <p>{{ $assignment->emp_id }}</p> --}}
-            @endforeach
-            <hr>
+                    <tr>
+                        <th>Shift</th>
+                        <th>Role</th>
+                        <th>Care Group</th>
+                        <th>Employee Name</th>
+                    </tr>
+                    @foreach ($schedule->assignments as $assignment)
+                    <tr>
+                        <td>{{ strtoupper($assignment->shift) }}</td>
+                        <td>{{ $assignment->role }}</td>
+                        <td>{{ $assignment->care_group }}</td>
+                        <td>
+                            {{ $assignment->employee->user->fname }}
+                            {{ $assignment->employee->user->lname }}
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>    
+            {{-- <hr> --}}
         @endforeach
     </div>
 @else
